6048e2f9f7376f4176804ff500d898233d2901d4
F-ALWAISI <f.alwaisi23@gmail.com>
2025-11-08 18:32:13 +0100

    Update galerie.js

--- js/galerie.js
+++ js/galerie.js
@@ -176,7 +176,7 @@ const artworks = {
         category: "etain",
         year: "2022",
         dimensions: "30x40cm",
-        image: "images/oeuvres/etain/la-naissance-de-jesus.jpg",
+        image: "images/oeuvres/etain/la-naissance-de-jesus.JPG",
     },
     masque_venitien: {
         title: "masque-venitien",
@@ -992,6 +992,7 @@ document.addEventListener('keydown', function(e) {
 });
 
 devLog('🔒 Protection des images activée');
+
 
 
 
